18dd803045a41d6eb02405b546a819b52d471cad
dsanchez333 <daniel.sanchez1@insbaixcamp.cat>
2024-02-19 21:12:10 +0100

    Correción de xss basic-stored editando index.php i stored.php añadiendo función para comprobar caracteres especiales de códigos JavaScript

--- app/lab/xss/basic-stored/stored.php
+++ app/lab/xss/basic-stored/stored.php
@@ -5,41 +5,28 @@ $strings = tr();
 $db = new PDO('sqlite:database.db');
 
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-    <title><?php echo $strings['title']; ?></title>
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+
+  <title><?php echo htmlspecialchars($strings['title']); ?></title>
 </head>
 
 <body>
-    <div class="alert alert-primary d-flex justify-content-center" style="text-align: center;width: fit-content;margin: auto;margin-top: 3vh;">
-        <h6><?php echo $strings['text']; ?></h6>
-    </div>
-    <div class="container d-flex justify-content-center">
-        <div class="wrapper col-md-6  shadow-lg" style="border-radius: 15px; margin-top: 4vh;">
-            <div class="shadow-sm m-2 scrollspy-example chat-log d-flex flex-column justify-content-end align-items-end overflow-auto" style="min-height: 350px;border: rgb(206, 206, 206) 1px solid; border-radius: 15px;" data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-offset="0" tabindex="0">
-                <?php
-                session_start();
-                if (!isset($_SESSION['username'])) {
-                    header("Location: index.php");
-                    exit;
-                }
-                $uname = $_SESSION['username'];
+  <div class="container d-flex justify-content-center">
+    <div class="shadow p-3 mb-5 rounded column" style="text-align: center; max-width: 1000px;margin-top:15vh;">
+      <h4>VULNLAB</h4>
 
-                $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-
-                $q = $db->query("SELECT * FROM mandalorian_content");
-
-                if ($q) {
-                    while ($cikti = $q->fetch(PDO::FETCH_ASSOC)) {
+      <form action="#" method="POST" style="text-align: center;margin-top: 20px;padding:30px;">
+        <button type="submit" class="btn btn-primary"><?php echo htmlspecialchars($strings['logout']); ?></button> <!-- Botón de cierre de sesión -->
+      </form>
 
                         echo '<div class="msg col-md-6 m-3 px-4 bg-primary text-wrap " style="border-radius: 20px; padding: 5px;width: fit-content;color: aliceblue;">';
                         echo $cikti['content'];
@@ -48,42 +35,21 @@ $db = new PDO('sqlite:database.db');
                 }
                 #}
 
-                ?>
+            </div>
             </div>
             <div class="p-3 pb-0" style="text-align: center;">
                 <form action="#" method="POST" style="margin: 0;">
                     <textarea placeholder="<?php echo $strings['message']; ?>" class="form-control" rows="3" name="mes"></textarea>
                     <button type="submit" class="btn btn-primary m-3"><?php echo $strings['submit']; ?></button>
                 </form>
-            </div>
-        </div>
     </div>
-    <form action="#" method="post">
-        <button type="submit" name="del" class="btn btn-primary m-3"><?php echo $strings['delete']; ?></button>
-    </form>
-
-    <?php
-
-    if (isset($_POST['del'])) {
-        $q = $db->prepare("DELETE FROM `mandalorian_content`");
-        $q->execute();
-
-        header("Location: stored.php");
-        exit;
-    }
-    if (isset($_POST['mes'])) {
-        $q = $db->prepare("INSERT INTO mandalorian_content (username,content) VALUES (:username,:message)");
-        $q->execute(array(
-            "username" => $_SESSION['username'],
-            "message" => $_POST['mes'],
-        ));
-        header("Location: stored.php");
-        exit;
-    }
-
-    ?>
-    </div>
-    <script id="VLBar" title="<?= $strings['title'] ?>" category-id="1" src="/public/assets/js/vlnav.min.js"></script>
+            <div class="p-3 pb-0" style="text-align: center;">
+                <form action="#" method="POST" style="margin: 0;">
+                    <textarea placeholder="<?php echo $strings['message']; ?>" class="form-control" rows="3" name="mes"></textarea>
+                    <button type="submit" class="btn btn-primary m-3"><?php echo $strings['submit']; ?></button>
+                </form>
+  </div>
+  <script id="VLBar" title="<?= htmlspecialchars($strings['title']) ?>" category-id="1" src="/public/assets/js/vlnav.min.js"></script>
 </body>
 
 </html>
