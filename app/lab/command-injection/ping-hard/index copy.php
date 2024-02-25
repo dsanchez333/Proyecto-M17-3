91842ba60082b50b65ba21d62f4e22ffde73212e
dsanchez333 <daniel.sanchez1@insbaixcamp.cat>
2024-02-25 22:05:46 +0100

    Correción vulnerabilidad command injection "hard ping" añadiendo validación de caracteres

--- app/lab/command-injection/ping-hard/index copy.php
+++ app/lab/command-injection/ping-hard/index copy.php
@@ -5,16 +5,13 @@ $strings = tr();
 
 ?>
 <!DOCTYPE HTML>
-
 <html lang="en-US">
 
 <head>
-
 	<meta charset="UTF-8">
-
-	<title>PING</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?= $strings['title1']; ?></title>
 	<link rel="stylesheet" href="./../bootstrap.min.css">
-
 </head>
 
 <body>
@@ -30,30 +27,39 @@ $strings = tr();
 				</form>
 			</div>
 
-			<div class="col-md-auto d-flex justify-content-center" style="">
+		<div class="col-md-auto d-flex justify-content-center" style="">
 
-				<?php
-				if (isset($_POST["ip"])) {
-					$input = $_POST["ip"];
-					echo "<br /><br />";
+			<?php
+			if (isset($_POST["ip"])) {
+				$input = $_POST["ip"];
+				$blacklists = array(" ", "&", ";", "@", "%", "^", "'", "<", ">", ",", "\\", "/", "ls", "cat", "less", "tail", "more", "whoami", "pwd", "echo", "ps");
+				$arraySize = sizeof($blacklists);
+				$status = 0;
 
+				foreach ($blacklists as $blacklist) {
+					if (!strstr($input, $blacklist)) {
+						//$input = str_replace($blacklist,"", $input);
+						$status++;
+					}
+				}
+				if ($arraySize == $status) {
 					exec("ping -c5 $input", $out);
 					if (!empty($out)) {
-
 						echo '<div class="mt-5 alert alert-primary" role="alert" style=" width:500px;" > <strong>  <p style="text-align:center;">';
 						foreach ($out as $line) {
-
 							echo $line;
 							echo "<br>";
 						}
 						echo ' </p></strong></div>';
 					}
+				} else {
+					echo '<div class="mt-5 alert alert-danger" role="alert" style=" width:500px;" > <strong>  <p style="text-align:center;">ERROR</p></strong></div>';
 				}
-
-				?>
-			</div>
+			}
+			?>
 		</div>
 	</div>
+</div>
 	<script id="VLBar" title="<?= $strings['title1'] ?>" category-id="4" src="/public/assets/js/vlnav.min.js"></script>
 </body>
 
