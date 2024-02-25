2c41aae689b2fe3ced694a41882267670de46f0d
Kbsoide21 <daanielrojo@gmail.com>
2024-02-25 18:22:18 +0100

    AAAAAAAAAAAAAAAAAAAAAAAAA

--- app/lab/idor/invoices/index.php
+++ app/lab/idor/invoices/index.php
@@ -5,6 +5,11 @@
 
     $db = new PDO('sqlite:database.db'); 
 
+    $sql = “SELECT * FROM users WHERE email=?;“; //Declaraciones para conectarse a la db
+    $stmt = mysqli_stmt_init($conn);
+    mysqli_stmt_bind_param($stmt, “s”, $email);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
     $_SESSION[‘uid’] = $row[‘uid’];
     $user_id = $_SESSION[‘uid’];
     $user_info = get_user_info($user_id);
