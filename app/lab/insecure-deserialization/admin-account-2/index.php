8d1c01326de96843e685d4b541fc134f740d6eff
Ramon-03 <ramon.gabarri1@insbaixcamp.cat>
2024-02-25 20:16:32 +0100

    admin-account2_RESET 0

--- app/lab/insecure-deserialization/admin-account-2/index.php
+++ app/lab/insecure-deserialization/admin-account-2/index.php
@@ -5,17 +5,12 @@ require("../../../lang/lang.php");
 $strings = tr();
 error_reporting(0);
 ini_set('display_errors', 0);
-
-if (isset($_COOKIE['d2VsY29tZS1hZG1pbmlzdHJhdG9y'])) {
+if( isset($_COOKIE['d2VsY29tZS1hZG1pbmlzdHJhdG9y']) ){
     $user;
-    try {
-        $encodedUserData = base64_decode(urldecode($_COOKIE['d2VsY29tZS1hZG1pbmlzdHJhdG9y']));
-        $user = json_decode($encodedUserData);
-
-        if ($user === null) {
-            throw new Exception("Error decoding JSON data");
-        }
-    } catch (Exception $e) {
+    try{
+    $user = unserialize( urldecode( base64_decode ( $_COOKIE['d2VsY29tZS1hZG1pbmlzdHJhdG9y'] ) ));
+   
+    }catch(Exception $e){
         header("Location: login.php?msg=3");
         exit;
     }
@@ -23,25 +18,26 @@ if (isset($_COOKIE['d2VsY29tZS1hZG1pbmlzdHJhdG9y'])) {
     $text = "";
     $administrator = md5('administrator');
     $admin = md5('admin');
-
-    if ($user->isAdmin == '1') {
-        if ($user->username === $administrator)
-            $text = $strings['welcome-real-admin'];
-        else if ($user->username === $admin)
-            $text =  $strings['welcome-fake-admin'];
+    if( $user->isAdmin == '1' ){
+        if( $user->username === $administrator )
+        $text = $strings['welcome-real-admin'];
+        else if( $user->username === $admin )
+        $text =  $strings['welcome-fake-admin'];
         else
-            $text = $strings['welcome-fake-admin-user'];
-    } else if ($user->username === $administrator || $user->username === $admin) {
+        $text = $strings['welcome-fake-admin-user'];
+    }else if( $user->username === $administrator || $user->username === $admin ){
         $text = $strings['welcome-admin-powerless'];
-    } else {
+    }else{
         $text = $strings['welcome-test'];
     }
-} else {
+   
+
+}else{
     header("Location: login.php?msg=2");
     exit;
 }
-?>
 
+?>
 
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <!------ Include the above in your HEAD tag ---------->
