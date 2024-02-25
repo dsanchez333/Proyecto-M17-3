da1cad5100c8f1e48a4f6ddff47580ebedeb9c7a
Ramon-03 <ramon.gabarri1@insbaixcamp.cat>
2024-02-25 20:49:31 +0100

    correció index.php-full-privileges

--- app/lab/insecure-deserialization/full-privileges/index.php
+++ app/lab/insecure-deserialization/full-privileges/index.php
@@ -3,8 +3,10 @@
 include("user.php");
 include("permission.php");
 require("../../../lang/lang.php");
+
 error_reporting(0);
 ini_set('display_errors', 0);
+
 $strings = tr();
 $user;
 
@@ -12,16 +14,18 @@ if (isset($_COOKIE['Z3JhbnQtZnVsbC1wcml2aWxpZ2VzCg'])) {
     try {
         $user = unserialize(urldecode(base64_decode($_COOKIE['Z3JhbnQtZnVsbC1wcml2aWxpZ2VzCg'])));
     } catch (Exception $e) {
-        header("Location: login.php?msg=3");
-        exit;
+        redirectToLogin();
     }
 } else {
+    redirectToLogin();
+}
+
+function redirectToLogin() {
     header("Location: login.php?msg=2");
     exit;
 }
 
-function canDo($action, $strings)
-{
+function canDo($action, $strings) {
     return $action === 1 ? $strings['yes-sir'] : $strings['no-sir'];
 }
 
@@ -30,36 +34,38 @@ function canDo($action, $strings)
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <!------ Include the above in your HEAD tag ---------->
 
-<!DOCTYPE html><html lang='en' class=''>
+<!DOCTYPE html>
+<html lang='en' class=''>
 <head>
-<style>
-h1{
-    text-align: center;
- }
-</style>
-<link rel='stylesheet prefetch' href='css/normalize.min.css'><script src='js/prefixfree.min.js'></script>
-</head><body>
-<div style = "text-align:middle">
-<?php
-  echo "<h1>";
-  echo $strings['welcome-test'];
-  echo "</h1>";
- 
-  echo "<h1>";
-  echo $strings["what-can-you-do"];
-  echo "</h1>";
-  $permissions = $user->permissions;
-  $delete = $permissions->canDelete;
-  $update = $permissions->canUpdate;
-  $add = $permissions->canAdd;
-  echo "<h1>".$strings['delete']." : ".canDo($delete,$strings)."</h1>";
-  echo "<h1>".$strings['update']." : ".canDo($update,$strings)."</h1>";
-  echo "<h1>".$strings['add']." : ".canDo($add,$strings)."</h1>";
-  if( $delete === 1 && $add === 1 & $update === 1){
-      echo "<h1>".$strings['you-have-all-priviliges']."</h1>";;
-  }
-?>
-</div>
+    <style>
+        h1 {
+            text-align: center;
+        }
+    </style>
+    <link rel='stylesheet prefetch' href='css/normalize.min.css'>
+    <script src='js/prefixfree.min.js'></script>
+</head>
+<body>
+    <div style="text-align: middle">
+        <?php
+        echo "<h1>";
+        echo $strings['welcome-test'];
+        echo "</h1>";
+
+        echo "<h1>";
+        echo $strings["what-can-you-do"];
+        echo "</h1>";
+        $permissions = $user->permissions;
+        $delete = $permissions->canDelete;
+        $update = $permissions->canUpdate;
+        $add = $permissions->canAdd;
+        echo "<h1>" . $strings['delete'] . " : " . canDo($delete, $strings) . "</h1>";
+        echo "<h1>" . $strings['update'] . " : " . canDo($update, $strings) . "</h1>";
+        echo "<h1>" . $strings['add'] . " : " . canDo($add, $strings) . "</h1>";
+        if ($delete === 1 && $add === 1 & $update === 1) {
+            echo "<h1>" . $strings['you-have-all-priviliges'] . "</h1>";
+        }
+        ?>
+    </div>
 </body>
-<script id="VLBar" title="<?= $strings['title']; ?>" category-id="9" src="/public/assets/js/vlnav.min.js"></script>
 </html>
