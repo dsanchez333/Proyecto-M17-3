988b8dc41c393e50e598a700b7866b503e92651a
Ramon-03 <ramon.gabarri1@insbaixcamp.cat>
2024-02-25 18:13:26 +0100

    Tornem als originals

--- app/lab/insecure-deserialization/admin-account-1/index.php
+++ app/lab/insecure-deserialization/admin-account-1/index.php
@@ -1,69 +1,50 @@
 <?php
+ 
 include("user.php");
 require("../../../lang/lang.php");
 $strings = tr();
 error_reporting(0);
 ini_set('display_errors', 0);
 
-// Define a secret key for signing the cookie
-$secretKey = 'your_secret_key_here';
-
-if (isset($_COOKIE['V2VsY29tZS1hZG1pbgo'])) {
-    try {
-        // Decrypt and verify the cookie using the secret key
-        $cookieValue = $_COOKIE['V2VsY29tZS1hZG1pbgo'];
-        $signature = hash_hmac('sha256', $cookieValue, $secretKey);
-        $user = unserialize(base64_decode($cookieValue));
-
-        // Debugging information - remove this in production
-        echo "Received Cookie Value: " . $cookieValue . "<br>";
-        echo "Calculated Signature: " . $signature . "<br>";
-        echo "Stored Signature: " . $user->signature . "<br>";
-        echo "User Object:<br>";
-        print_r($user);
-
-        if (!hash_equals($user->signature, $signature)) {
-            // If the signature doesn't match, redirect to login page
-            header("Location: login.php?msg=3");
-            exit;
-        }
-    } catch (Exception $e) {
-        // Debugging information - remove this in production
-        echo "Exception: " . $e->getMessage() . "<br>";
-
+if( isset($_COOKIE['V2VsY29tZS1hZG1pbgo']) ){
+    try{
+    $user = unserialize( base64_decode( $_COOKIE['V2VsY29tZS1hZG1pbgo'] ));
+    }catch(Exception $e){
         header("Location: login.php?msg=3");
         exit;
-    }
-
+    } 
     $text = "";
-    if ($user->username === "admin") {
+    if( $user->username === "admin"){
         $text = $strings['welcome-admin'];
-    } else if ($user->username === "test") {
+    } else if ( $user->username === "test"){
         $text = $strings['welcome-test'];
-    } else {
+    }else{
         $text =  $strings['welcome-another'];
     }
-} else {
+
+}else{
     header("Location: login.php?msg=2");
     exit;
 }
+
 ?>
 
+
+ 
 <!------ Include the above in your HEAD tag ---------->
 
-<!DOCTYPE html>
-<html lang='en' class=''>
+<!DOCTYPE html><html lang='en' class=''>
 <head>
-    <style>
-        h1 {
-            text-align: center;
-        }
-    </style>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-</head>
-<body>
+<style>
+h1{
+    text-align: center;
+ }
+</style>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ 
+</head><body>
 
-<?php echo '<h2 style="text-align: center; color:red; margin-top: 100px;">' . $text . '</h2>'; ?>
+<?php echo '<h2 style="text-align: center; color:red; margin-top: 100px;">'.$text.'</h2>'; ?>
 
 </body>
 <script id="VLBar" title="<?= $strings['title']; ?>" category-id="9" src="/public/assets/js/vlnav.min.js"></script>
--- app/lab/insecure-deserialization/admin-account-1/login.php
+++ app/lab/insecure-deserialization/admin-account-1/login.php
@@ -1,42 +1,38 @@
 <?php
-require("user.php");
-require("db.php");
-require("../../../lang/lang.php");
-$strings = tr();
 
-$db = new DB();
-$users = $db->getUsersList();
+	require("user.php");
+	require("db.php");
+    require("../../../lang/lang.php");
+	$strings = tr();
+ 
+	$db = new DB();
+	$users = $db->getUsersList();
+	
 
-if (isset($_POST['username']) && isset($_POST['password'])) {
-    $inputUsername = $_POST['username'];
-    $inputPassword = $_POST['password'];
+	if( isset( $_POST['username'] ) && isset( $_POST['password'] ) ){
+		
+		$username = $users[0]['username'];
+		$password = $users[0]['password'];
+ 
+		if( $username === $_POST['username'] && $password === $_POST['password'] ){
+		 
+			 
+			header("Location: index.php");
+			$user = new User($username,$password);
+			$serializedStr = serialize($user);
+			$extremeSecretCookie = base64_encode($serializedStr);
+			setcookie('V2VsY29tZS1hZG1pbgo',$extremeSecretCookie);
+			
+			header("Location: index.php");
+			exit;
+		}
+		else{
+			header("Location: login.php?msg=1");
+			exit;
+		}
+	}
 
-    // Check if the user exists
-    $userExists = false;
-    foreach ($users as $user) {
-        if ($user['username'] === $inputUsername && $user['password'] === $inputPassword) {
-            $userExists = true;
-            break;
-        }
-    }
-
-    if ($userExists) {
-        // User exists, set the cookie and redirect
-        $user = new User($inputUsername, $inputPassword);
-        $serializedStr = serialize($user);
-        $extremeSecretCookie = base64_encode($serializedStr);
-        setcookie('V2VsY29tZS1hZG1pbgo', $extremeSecretCookie);
-
-        header("Location: index.php");
-        exit;
-    } else {
-        // Invalid credentials, redirect with error message
-        header("Location: login.php?msg=1");
-        exit;
-    }
-}
 ?>
-
 
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 <!------ Include the above in your HEAD tag ---------->
