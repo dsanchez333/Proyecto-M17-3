d6e8571cf3cfecb82243149b3991544940c75a91
Ramon-03 <ramon.gabarri1@insbaixcamp.cat>
2024-02-26 16:35:26 +0100

    SSTI-Basic-Utilització-htmlspecialchars

--- app/lab/ssti/SSTI-Basic/index.php
+++ app/lab/ssti/SSTI-Basic/index.php
@@ -25,15 +25,15 @@ try {
 <body>
 
     <div class="container" style="padding-top:5%;">
-        <h1 class="mb-2 text-center"><?php echo $strings['welcome']; ?></h1><br>
+        <h1 class="mb-2 text-center"><?php echo htmlspecialchars($strings['welcome']); ?></h1><br>
 
         <form method="POST" class="text-center">
             <div class="mb-2">
-                <label for="content" class="form-label"><?php echo $strings['blog_post']; ?></label>
-                <textarea class="form-control" id="content" name="content" rows="6" placeholder="<?php echo $strings['who']; ?>" required></textarea>
+                <label for="content" class="form-label"><?php echo htmlspecialchars($strings['blog_post']); ?></label>
+                <textarea class="form-control" id="content" name="content" rows="6" placeholder="<?php echo htmlspecialchars($strings['who']); ?>" required></textarea>
             </div>
 
-            <button type="submit" class="btn btn-primary" name="submit"><?php echo $strings['submit']; ?></button>
+            <button type="submit" class="btn btn-primary" name="submit"><?php echo htmlspecialchars($strings['submit']); ?></button>
         </form><br>
         <?php
 
@@ -42,9 +42,9 @@ try {
                 require '../../../public/vendor/autoload.php';
                 $loader = new Twig\Loader\ArrayLoader(['user_input' => strip_tags($_POST["content"])]);
                 $twig = new Twig\Environment($loader);
-                $escapedInput = $twig->render('user_input');
+                $escapedInput = htmlspecialchars($twig->render('user_input'));
             } catch (Exception $e) {
-                echo ('ERROR:' . $e->getMessage());
+                echo ('ERROR:' . htmlspecialchars($e->getMessage()));
             }
 
             $query = "INSERT INTO blog_entries (content) VALUES (:content)";
@@ -58,14 +58,14 @@ try {
         $stmt->execute();
         $entries = $stmt->fetchAll(PDO::FETCH_ASSOC);
         ?>
-        <h2 class="mt-2 text-center"><?php echo $strings['latest_entries']; ?></h2>
+        <h2 class="mt-2 text-center"><?php echo htmlspecialchars($strings['latest_entries']); ?></h2>
         <div id="blogEntries" class="row">
             <?php
             foreach ($entries as $entry) {
                 echo '<div class="col-md-12 mb-4">';
                 echo '<div class="card shadow">';
                 echo '<div class="card-body">';
-                echo '<p class="card-text">' . $entry['content'] . '</p>';
+                echo '<p class="card-text">' . htmlspecialchars($entry['content']) . '</p>';
                 echo '</div>';
                 echo '</div>';
                 echo '</div>';
@@ -75,7 +75,8 @@ try {
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
-    <script id="VLBar" title="<?= $strings["title"]; ?>" category-id="12" src="/public/assets/js/vlnav.min.js"></script>
+    <script id="VLBar" title="<?= htmlspecialchars($strings["title"]); ?>" category-id="12" src="/public/assets/js/vlnav.min.js"></script>
 </body>
 
 </html>
+
